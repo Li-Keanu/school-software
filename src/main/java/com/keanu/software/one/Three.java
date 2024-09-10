44ef108036c3c2086cffe889b5afdf56ec957c2a
keanu <3352103646@qq.com>
2024-09-10 10:45:03 +0800

    完成实验一/03

--- src/main/java/com/keanu/software/one/Three.java
+++ src/main/java/com/keanu/software/one/Three.java
@@ -1,12 +1,17 @@
 package com.keanu.software.one;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * <p>
  *  写一个命令行程序。
  *  要求：输出1~20000内的所有素数，按每行5个打印出来，并分析程序中最费时的函数是什么， 如何改进？
+ * </p>
+ *
+ * <p>
+ *      最费时的函数是 isPrime 函数，因为它需要迭代从 2 到 Math.sqrt(num) 的所有数字，
+ *  并检查是否能被 num 整除。该函数的时间复杂度为 O(sqrt(n))，随着数字的增加，
+ *  该函数的执行时间也会增加。
+ *      只需要检查num % i == 0的情况，其中i为不大于 Math.sqrt(num) 的素数。
+ *  这是因为如果存在一个大于 Math.sqrt(num) 的因子，那么必然存在一个小于 Math.sqrt(num) 的因子。
  * </p>
  *
  * @author <a href="https://github.com/Li-Keanu">keanu</a>
