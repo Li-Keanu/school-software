582d8ccc5f84e2f7c44de869f7231bbb57677975
keanu <3352103646@qq.com>
2024-09-10 10:15:03 +0800

    完成实验一/01

---
+++ src/main/java/com/keanu/software/one/One.java
@@ -0,0 +1,15 @@
+package com.keanu.software.one;
+
+/**
+ * <p>
+ *  编程实现 Hello World
+ * </p>
+ *
+ * @author <a href="https://github.com/Li-Keanu">keanu</a>
+ * @since 2024/9/10 10:13
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/keanu/software/one/Two.java
@@ -0,0 +1,12 @@
+package com.keanu.software.one;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author <a href="https://github.com/Li-Keanu">keanu</a>
+ * @since 2024/9/10 10:14
+ */
+public class Two {
+}
